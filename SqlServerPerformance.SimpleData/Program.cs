4a3c86c292901a8a09ba1515ca8bb468cf56a0b1
danielmarbach <daniel.marbach@openplace.net>
2013-03-14 23:09:40 +0100

    Added Simple.Data and Sql Server

---
+++ SqlServerPerformance.SimpleData/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Simple.Data;
+
+namespace SqlServerPerformance.SimpleData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            int numberOfDocuments = Convert.ToInt32(args[0]);
+
+            DocumentsWithTransactionPerDocument(Database.Open(), numberOfDocuments, stopWatch);
+            DocumentsWithTransactionForAllDocument(Database.Open(), numberOfDocuments, stopWatch);
+            DocumentsWithBulkInsert(Database.Open(), numberOfDocuments, stopWatch);
+        }
+
+        private static void DocumentsWithTransactionPerDocument(dynamic database, int numberOfDocuments, Stopwatch stopWatch)
+        {
+            Console.WriteLine("Writing {0} with transaction per document...", numberOfDocuments);
+
+            stopWatch.Start();
+
+            for (int i = 0; i < numberOfDocuments; i++)
+            {
+                using (var tx = database.BeginTransaction())
+                {
+                    tx.Users.Insert(new Data { Counter = i });
+
+                    tx.Commit();
+                }
+            }
+
+            stopWatch.Stop();
+
+            SaveStats(database, numberOfDocuments, stopWatch, "Documents with transaction per document.");
+
+            Console.WriteLine("Writing {0} with transaction per document {1}, {2} docs/sec", numberOfDocuments,
+                              stopWatch.ElapsedMilliseconds, numberOfDocuments*1000/stopWatch.ElapsedMilliseconds);
+
+            stopWatch.Reset();
+        }
+
+        private static void DocumentsWithTransactionForAllDocument(dynamic database, int numberOfDocuments, Stopwatch stopWatch)
+        {
+            Console.WriteLine("Writing {0} with transaction for all document...", numberOfDocuments);
+
+            stopWatch.Start();
+
+            using (var tx = database.BeginTransaction())
+            {
+                for (int i = 0; i < numberOfDocuments; i++)
+                {
+                    tx.Users.Insert(new Data { Counter = 1});
+                }
+
+                tx.Commit();
+            }
+
+            stopWatch.Stop();
+
+            SaveStats(database, numberOfDocuments, stopWatch, "Documents with transaction for all document.");
+
+            Console.WriteLine("Writing {0} with transaction for all document {1}, {2} docs/sec", numberOfDocuments,
+                              stopWatch.ElapsedMilliseconds, numberOfDocuments * 1000 / stopWatch.ElapsedMilliseconds);
+
+            stopWatch.Reset();
+        }
+
+        private static void DocumentsWithBulkInsert(dynamic database, int numberOfDocuments, Stopwatch stopWatch)
+        {
+            Console.WriteLine("Writing {0} with bulk insert...", numberOfDocuments);
+
+            stopWatch.Start();
+
+            var datas = new List<Data>();
+
+            for (int i = 0; i < numberOfDocuments; i++)
+            {
+                datas.Add(new Data { Counter = 1 });
+            }
+
+            using (var tx = database.BeginTransaction())
+            {
+                tx.Users.Insert(datas);
+
+                tx.Commit();
+            }
+
+            stopWatch.Stop();
+
+            SaveStats(database, numberOfDocuments, stopWatch, "Documents with bulk insert.");
+
+            Console.WriteLine("Writing {0} with bulk insert {1}, {2} docs/sec", numberOfDocuments,
+                              stopWatch.ElapsedMilliseconds, numberOfDocuments * 1000 / stopWatch.ElapsedMilliseconds);
+
+            stopWatch.Reset();
+        }
+
+        private static void SaveStats(dynamic database, int numberOfDocuments, Stopwatch stopWatch, string description)
+        {
+            using (var tx = database.BeginTransaction())
+            {
+                var stats = new Stats(description, numberOfDocuments, stopWatch.ElapsedMilliseconds);
+
+                tx.Stats.Insert(stats);
+
+                tx.Commit();
+            }
+        }
+
+        public class Data
+        {
+            public long Counter { get; set; }
+        }
+
+        public class Stats
+        {
+            public Stats(string description, long numberOfDocs, long timeInMs)
+            {
+                this.TimeInMs = timeInMs;
+                this.NumberOfDocuments = numberOfDocs;
+                this.At = DateTimeOffset.Now;
+                this.Description = description;
+                this.DocsPerSecond = timeInMs == 0 ? -1 : numberOfDocs * 1000 / timeInMs;
+            }
+
+            public string Description { get; private set; }
+
+            public long NumberOfDocuments { get; private set; }
+
+            public DateTimeOffset At { get; private set; }
+
+            public long TimeInMs { get; private set; }
+
+            public long DocsPerSecond { get; private set; }
+        }
+    }
+}
---
+++ SqlServerPerformance.SimpleData/Tables.sql
@@ -0,0 +1,49 @@
+﻿USE [PerformanceTestSimpleData]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 3/14/2013 11:02:17 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Counter] [bigint] NOT NULL,
+ CONSTRAINT [PK_Users_1] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+USE [PerformanceTestSimpleData]
+GO
+
+/****** Object:  Table [dbo].[Stats]    Script Date: 3/14/2013 11:03:31 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Stats](
+	[TimeInMs] [bigint] NOT NULL,
+	[Description] [varchar](255) NOT NULL,
+	[NumberOfDocuments] [bigint] NOT NULL,
+	[DocsPerSecond] [bigint] NOT NULL,
+	[At] [datetimeoffset](7) NOT NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
+
